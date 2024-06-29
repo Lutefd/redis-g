2c261dba4e0298142ef37f8f5be42836fe7e3be3
Luis Dourado <lfaddourado@gmail.com>
2024-06-29 17:23:53 -0300

    improve formatting and add mock clients for testing

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"log/slog"
 	"net"
+	"time"
+
+	"github.com/lutefd/redis-g/client"
 )
 
 const defaultListenAddr = ":5001"
@@ -12,24 +16,24 @@ type Config struct {
 	ListenAddr string
 }
 type Server struct {
-	Config 
-	ln net.Listener
-	peers map[*Peer]bool
+	Config
+	ln        net.Listener
+	peers     map[*Peer]bool
 	addPeerCh chan *Peer
-	quitCh chan struct{}
-	msgCh chan []byte
+	quitCh    chan struct{}
+	msgCh     chan []byte
 }
 
-func NewServer (cfg Config) *Server {
-	if len(cfg.ListenAddr) == 0{
+func NewServer(cfg Config) *Server {
+	if len(cfg.ListenAddr) == 0 {
 		cfg.ListenAddr = defaultListenAddr
 	}
 	return &Server{
-		Config: cfg,
-		peers: make(map[*Peer]bool),
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
 		addPeerCh: make(chan *Peer),
-		quitCh: make(chan struct{}),
-		msgCh: make(chan []byte),
+		quitCh:    make(chan struct{}),
+		msgCh:     make(chan []byte),
 	}
 }
 
@@ -45,35 +49,35 @@ func (s *Server) Start() error {
 	return s.acceptLoop()
 }
 
-func (s *Server) handleRawMessage (rawMsg []byte) error {
+func (s *Server) handleRawMessage(rawMsg []byte) error {
 	cmd, err := parseCommand(string(rawMsg))
 	if err != nil {
 		return err
 	}
-	switch v:=  cmd.(type) {
+	switch v := cmd.(type) {
 	case SetCommand:
-		slog.Info("somebody wants to set a key into the hash table", "key", v.key, "value", v.val )
+		slog.Info("somebody wants to set a key into the hash table", "key", v.key, "value", v.val)
 	}
 	return nil
 }
 
-func (s *Server) loop(){
+func (s *Server) loop() {
 	for {
-		select{
-		case rawMsg := <- s.msgCh:
+		select {
+		case rawMsg := <-s.msgCh:
 			if err := s.handleRawMessage(rawMsg); err != nil {
-					slog.Error("raw message decoding error", "err", err)
+				slog.Error("raw message decoding error", "err", err)
 
 			}
-		case peer := <- s.addPeerCh:
-			s.peers[peer] = true 
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
 		case <-s.quitCh:
 			return
 		}
 	}
 }
 
-func (s *Server) acceptLoop() error{
+func (s *Server) acceptLoop() error {
 	for {
 		conn, err := s.ln.Accept()
 		if err != nil {
@@ -84,17 +88,28 @@ func (s *Server) acceptLoop() error{
 	}
 }
 
-func (s *Server) handleConn(conn net.Conn){
+func (s *Server) handleConn(conn net.Conn) {
 	peer := NewPeer(conn, s.msgCh)
 	s.addPeerCh <- peer
 	//block bc of errs
 	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
-	if err := peer.readLoop(); err !=nil {
+	if err := peer.readLoop(); err != nil {
 		slog.Error("peer read error", "err", err, "remoteAddr", conn.RemoteAddr())
 	}
 }
 
 func main() {
-	server := NewServer(Config{})
-	log.Fatal(server.Start())
+	go func() {
+		server := NewServer(Config{})
+		log.Fatal(server.Start())
+	}()
+	time.Sleep(time.Second)
+	for i := 0; i < 10; i++ {
+		c := client.New("localhost:5001")
+		if err := c.Set(context.TODO(), "foo", "bar"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	select {} //blocking so the program doesn't exit
 }
