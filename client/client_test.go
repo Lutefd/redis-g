ab407ed7b80289dccac2f576e5ab5781a7da7bcd
Luis Dourado <lfaddourado@gmail.com>
2024-06-30 20:35:23 -0300

    add client close and multiple clients test

--- client/client_test.go
+++ client/client_test.go
@@ -4,8 +4,38 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"testing"
 )
+
+func TestNewClients(t *testing.T) {
+
+	nClients := 10
+	wg := sync.WaitGroup{}
+	wg.Add(nClients)
+	for i := 0; i < nClients; i++ {
+		go func(it int) {
+			c, err := New("localhost:5001")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer c.Close()
+			key := fmt.Sprintf("client_key_%d", it)
+			value := fmt.Sprintf("client_val_%d", it)
+			if err := c.Set(context.TODO(), key, value); err != nil {
+				log.Fatal(err)
+			}
+			val, err := c.Get(context.TODO(), key)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("client_%d: GET => %s\n", it, val)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+}
 
 func TestNewClient(t *testing.T) {
 	c, err := New("localhost:5001")
